34a79dcfd94304455fa9aaf6ec9b8ba1497e012c
shuvoslashn <sarker3664@gmail.com>
2024-03-10 17:33:02 +0600

    banner background fixed

--- src/components/sections/Banner.jsx
+++ src/components/sections/Banner.jsx
@@ -1,6 +1,6 @@
 export default function Banner() {
     return (
-        <section className='mx-4 md:mx-24 py-16 md:py-40 bg-contact-banner rounded-2xl md:rounded-[40px] bg-center bg-cover bg-no-repeat text-center text-white'>
+        <section className='mx-4 md:mx-24 py-16 md:py-40 bg-black/60 bg-contact-banner rounded-2xl md:rounded-[40px] bg-center bg-cover bg-no-repeat text-center text-white'>
             <h2 className='text-4xl md:text-6xl font-bold'>Contact Us</h2>
             <p className='font-bold text-2xl mt-4'>Home {">"} Contact Us</p>
         </section>
